dc5e47e57f7c72181dbad9802b40c3e7af81cc72
Sergio Moreno <sergioams95@gmail.com>
2021-11-25 18:00:03 -0500

    config prueba

--- src/database/conexion.js
+++ src/database/conexion.js
@@ -1,11 +1,15 @@
 const { Pool } = require("pg")
 // Coloca aquí tus credenciales
 const pool = new Pool({
-  user: "enkcgzjvtnvvzd",
-  host: "ec2-44-199-49-128.compute-1.amazonaws.com",
-  database: "da6d6hctcil9pq",
-  password: "56aaac1c538ed4e280cad6e47c2219e717b25672bc6777e118808776d2b2431c",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+  // user: "enkcgzjvtnvvzd",
+  // host: "ec2-44-199-49-128.compute-1.amazonaws.com",
+  // database: "da6d6hctcil9pq",
+  // password: "56aaac1c538ed4e280cad6e47c2219e717b25672bc6777e118808776d2b2431c",
+  // port: 5432,
 });
 module.exports = pool;
 
